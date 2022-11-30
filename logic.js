4dc51e2f91dc18c98569b0c3b52766f6b4ed1f9d
kwakade03 <114900815+kwakade03@users.noreply.github.com>
2022-11-30 22:28:31 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,10 @@
+var a=0;
+function add() {
+  // let rem=document.getElementById("minus")
+        document.getElementById("num").innerHTML=a++;
+}
+function rem() {
+  // let rem=document.getElementById("minus")
+        document.getElementById("num").innerHTML=a--;
+}
+
